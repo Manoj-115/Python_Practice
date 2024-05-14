584afefbf6e4f5903c78a8d6903feb402594850e
Manoj <rmk1444@gmail.com>
2024-05-14 20:00:22 +0530

    changes

--- interchange_elements_in_list.py
+++
@@ -1,9 +0,0 @@
-#Python program to interchange first and last elements in a list
-
-def replace_firstandlast_element(list):
-    list[0], list[-1] = list[-1], list[0]
-    return list
-
-
-list = [23, 21, 42, 45, 67, 89]
-print(replace_firstandlast_element(list))
